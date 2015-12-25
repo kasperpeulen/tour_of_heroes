bf3c83ba9e2b5fdb955f6f3e09580bfb5125cf1e
Kasper Peulen <kasperpeulen@gmail.com>
2015-12-25 23:26:28 +0100

    fix: fix build

--- lib/hero_detail.component.dart
+++ lib/hero_detail.component.dart
@@ -6,7 +6,6 @@ import 'hero.dart';
     selector: 'hero-detail',
     templateUrl: 'hero_detail.component.html',
     directives: const [CORE_DIRECTIVES, FORM_DIRECTIVES],
-    styleUrls: const ['hero_detail.component.css'],
     encapsulation: ViewEncapsulation.None)
 class HeroDetailComponent {
   @Input() Hero hero;
